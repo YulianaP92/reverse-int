bba4e17dfd70a4fa65d23294edd64083976cfb81
Yuliana Polukhina <polushka.yulia@gmail.com>
2020-10-17 15:11:50 +0300

    feat: add revert function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+     let concat='';
+     let mas=n.toString();
+  for(let i=0; i<mas.length; i++){
+      if(mas[i]==='-'){
+          continue;
+      }
+    concat=mas[i]+concat;
+  }
+  return concat;
 }
